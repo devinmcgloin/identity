46c02b60831994de66dc807576e11e1e09082024
Devin McGloin <devin@devinmcgloin.com>
2018-09-20 00:16:04 +0100

    Added more gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,24 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     `gatsby-transformer-remark`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 850,
+              withWebp: true,
+              linkImagesToOriginal: true,
+            },
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -12,6 +30,22 @@ module.exports = {
         path: `${__dirname}/content/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Enables Google Optimize using your container Id
+        optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
+        // Any additional create only fields (optional)
+      },
+    },
+
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
