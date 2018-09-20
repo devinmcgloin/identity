20df894a47791ac31f3c0d3c236e371cd1d5f9bb
Devin McGloin <devin@devinmcgloin.com>
2018-09-20 20:00:40 +0100

    Filled out components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Devin McGloin',
+    siteUrl: `https://devinmcgloin.com`,
   },
   plugins: [
     'gatsby-plugin-react-helmet',
---
+++ src/pages/experiments.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const IndexPage = () => (
+  <React.Fragment>
+    <h1>Hi Welcome</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+  </React.Fragment>
+)
+
+export default IndexPage
