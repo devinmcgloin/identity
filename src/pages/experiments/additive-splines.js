807e169e539295a77bbeb1cf53eb0cbae5f2957e
Devin McGloin <devin@devinmcgloin.com>
2018-10-29 16:50:10 +0000

    Fixed safari rendering

--- src/pages/experiments/additive-splines.js
+++ src/pages/experiments/additive-splines.js
@@ -25,21 +25,22 @@ function Ring(x, y, radius, color) {
 }
 
 class AdditiveSplines extends Component {
-  state = {
-    rings: [],
-    maxIterations: 100,
-    cursorRadius: 0.3,
-    click: false,
-    color: 'rgba(100, 200, 200, 0.1)',
-    resolution: 60,
-    varyPower: 0.02,
-    M: new Matrix(),
-  };
+  constructor(props) {
+    super(props);
+    this.rings = [];
+    this.maxIterations = 100;
+    this.cursorRadius = 0.3;
+    this.click = false;
+    this.color = 'rgba(100, 200, 200, 0.1)';
+    this.resolution = 60;
+    this.varyPower = 0.02;
+    this.M = new Matrix();
+  }
 
   mountDatGUI = datgui => {
-    datgui.addColor(this.state, 'color');
-    datgui.add(this.state, 'maxIterations').step(1);
-    datgui.add(this.state, 'cursorRadius', 0.2, 0.7);
+    datgui.addColor(this, 'color');
+    datgui.add(this, 'maxIterations').step(1);
+    datgui.add(this, 'cursorRadius', 0.2, 0.7);
     datgui.add(this, 'addRandom');
     datgui.add(this, 'clearCanvas');
     datgui.add(this, 'download');
@@ -54,13 +55,13 @@ class AdditiveSplines extends Component {
   };
 
   printColors = () => {
-    this.state.rings.map(r => console.log(r.color));
+    this.rings.map(r => console.log(r.color));
   };
 
   vary = path => {
     var newPoints = [];
     var points = path.points,
-      varyPower = this.state.varyPower;
+      varyPower = this.varyPower;
 
     for (var i = 0; i < points.length; i++) {
       var p = points[i];
@@ -77,24 +78,22 @@ class AdditiveSplines extends Component {
   };
 
   randomStyle = () =>
-    `rgba(${randInt(0, 255)}.${randInt(0, 255)},${randInt(0, 255)},0.1)`;
+    `rgba(${randInt(0, 255)},${randInt(0, 255)},${randInt(0, 255)},0.1)`;
 
   handleClick = (w, h) => {
-    let canvas = this.canvas;
-    let { click } = this.state;
+    let { canvas, click } = this;
 
     if (canvas.cursor.z && !click) {
       var pixel = pixeltocord(w, h, [canvas.cursor.x, canvas.cursor.y, 0]);
-      this.state.rings.push(
-        new Ring(pixel[0], pixel[1], this.state.cursorRadius, this.state.color)
+      this.rings.push(
+        new Ring(pixel[0], pixel[1], this.cursorRadius, this.color)
       );
-      this.setState({ click: true });
-    } else this.setState({ click: false });
+      this.click = true;
+    } else this.click = false;
   };
 
   resetIterations = () => {
-    for (var i = 0; i < this.state.rings.length; i++)
-      this.state.rings[i].iterations = 0;
+    for (var i = 0; i < this.rings.length; i++) this.rings[i].iterations = 0;
   };
 
   resetPoints = ring => {
@@ -115,14 +114,14 @@ class AdditiveSplines extends Component {
   clearCanvas = () => {
     var canvas = document.getElementById('canvas'),
       ctx = canvas.getContext('2d');
-    this.state.rings = [];
+    this.rings = [];
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   };
 
   addRandom = (n = 5) => {
     for (var i = 0; i < n; i++) {
       let style = this.randomStyle();
-      this.state.rings.push(
+      this.rings.push(
         new Ring(
           Math.random() * 2 - 1,
           Math.random() * 2 - 1,
@@ -143,7 +142,7 @@ class AdditiveSplines extends Component {
   };
 
   draw = (ctx, w, h) => {
-    let { rings, maxIterations, resolution, M } = this.state;
+    let { rings, maxIterations, resolution, M } = this;
 
     this.handleClick(w, h);
 
