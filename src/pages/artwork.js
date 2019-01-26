6cb11da8ae9f4d9d5eb3901d604560745ac6f8a0
Devin McGloin <devin@devinmcgloin.com>
2019-01-26 21:04:48 +0000

    Updated artwork grid

--- src/pages/artwork.js
+++ src/pages/artwork.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import { HeaderLayout } from '../components/layout';
 import { graphql } from 'gatsby';
-import Grid from '../components/grid';
+import Gallery from 'react-photo-gallery';
 
 const IndexPage = ({ data }) => {
-  let images = data.allImageSharp.edges.map(e => e.node.fluid.src);
+  let images = data.allImageSharp.edges.map(e => {
+    return {
+      src: e.node.fluid.src,
+      width: e.node.fluid.presentationWidth,
+      height: e.node.fluid.presentationHeight,
+    };
+  });
   return (
     <HeaderLayout title="Artwork">
-      <Grid imageURLS={images} />
+      <Gallery photos={images} />
     </HeaderLayout>
   );
 };
 
 export const query = graphql`
   {
-    allImageSharp(filter: { original: { src: { regex: "/sail/" } } }) {
+    allImageSharp(filter: { original: { src: { regex: "/sketch/" } } }) {
       edges {
         node {
           id
           fluid(quality: 100) {
             src
-            sizes
+            presentationWidth
+            presentationHeight
           }
         }
       }
