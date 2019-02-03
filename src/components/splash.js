b72fb16f1523e07299b7600373b3be57addee119
Devin McGloin <devin.mcgloin@intercom.io>
2019-02-03 23:58:01 +0000

    show partial layout only on large screens

--- src/components/splash.js
+++ src/components/splash.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Splash = () => (
   <div className="cf feature-bg pa3">
     <div className="mw8 center">
-      <header className="w-100 w-two-thirds-ns pa3 pa5-ns">
+      <header className="w-100 w-two-thirds-l pa3 pa5-ns">
         <h2 className="lh-title f1-ns f2 b ma0 garamond i">
           I make products that help people to understand the world around them,
           and think of new solutions to difficult problems.
