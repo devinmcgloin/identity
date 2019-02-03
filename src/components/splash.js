ffc594587801be19990eecba91d3a226dd3ccfe1
Devin McGloin <devin.mcgloin@intercom.io>
2019-02-03 22:36:56 +0000

    fixed splash on mobile

--- src/components/splash.js
+++ src/components/splash.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Splash = () => (
   <div className="cf feature-bg pa3">
     <div className="mw8 center">
-      <header className="fl w-two-thirds pa3 ma3 ma5-l">
-        <h2 className="lh-title f1-ns f2 b mt0 garamond i">
+      <header className="w-100 w-two-thirds-ns pa3 pa5-ns">
+        <h2 className="lh-title f1-ns f2 b ma0 garamond i">
           I make products that help people to understand the world around them,
           and think of new solutions to difficult problems.
         </h2>
