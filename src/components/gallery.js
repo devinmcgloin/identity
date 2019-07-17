2d9ef10657164363e14ef59e6ae72eb85c13c51f
Devin McGloin <devin@devinmcgloin.com>
2019-07-17 23:42:08 +0100

    added lightbox

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,10 +1,30 @@
 import React, { useState } from 'react';
 import Img from 'gatsby-image';
-import Lightbox from 'react-images';
 import Measure from 'react-measure';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    background: '#eee',
+    border: 'none',
+    borderRadius: '2px',
+    padding: '32px',
+  },
+  overlay: {
+    background: 'rgba(0, 0, 0, 0.7)',
+  },
+};
 
 const Gallery = ({ images }) => {
   const [width, setWidth] = useState(1100);
+  const [modalVisible, setModalVisibility] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(0);
 
   let widthFactor;
   if (width >= 945) widthFactor = 0.33;
@@ -18,7 +38,14 @@ const Gallery = ({ images }) => {
   );
 
   let renderedImages = images.map((image, i) => (
-    <div key={i} className={'w-33-l w-50-m w-100 pa2'}>
+    <div
+      key={i}
+      className={'w-33-l w-50-m w-100 pa2'}
+      onClick={() => {
+        setSelectedImage(i);
+        setModalVisibility(true);
+      }}
+    >
       <div className="pa4 bg-light-gray">
         <Img className="flex justify-center align-center" fluid={image} />
       </div>
@@ -28,23 +55,40 @@ const Gallery = ({ images }) => {
   let maxHeight = totalHeight * (widthFactor + 0.01);
 
   return (
-    <Measure
-      bounds
-      onResize={rect => {
-        let { width } = rect.bounds;
-        setWidth(width);
-      }}
-    >
-      {({ measureRef }) => (
-        <div
-          ref={measureRef}
-          className="flex flex-wrap flex-column mw8 flex-start"
-          style={{ maxHeight: maxHeight }}
-        >
-          {renderedImages}
-        </div>
-      )}
-    </Measure>
+    <React.Fragment>
+      <Modal
+        isOpen={modalVisible}
+        onRequestClose={() => setModalVisibility(false)}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <Img
+          style={{
+            width: width * images[selectedImage].aspectRatio,
+            maxHeight: '80vh',
+            maxWidth: '80vw',
+          }}
+          fluid={images[selectedImage]}
+        />
+      </Modal>
+      <Measure
+        bounds
+        onResize={rect => {
+          let { width } = rect.bounds;
+          setWidth(width);
+        }}
+      >
+        {({ measureRef }) => (
+          <div
+            ref={measureRef}
+            className="flex flex-wrap flex-column mw8 flex-start"
+            style={{ maxHeight: maxHeight }}
+          >
+            {renderedImages}
+          </div>
+        )}
+      </Measure>
+    </React.Fragment>
   );
 };
 
