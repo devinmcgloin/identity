0af4c1a04ce6bde1104a609eaa459f3ac4bb185c
Devin McGloin <devin@devinmcgloin.com>
2020-06-14 14:15:28 +0100

    Basic tailwind

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Head from 'next/head';
+export default () => (
+  <div className="p-4 shadow rounded bg-gray-800">
+    <h1 className="text-purple-500 leading-normal">Next.js</h1>
+    <p className="text-gray-500">with Tailwind CSS</p>
+  </div>
+);
