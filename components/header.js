0582654b359f89d5ae1ce10c5a74c1008298978d
Devin McGloin <devin@devinmcgloin.com>
2020-10-17 16:02:56 +0100

    Line logo

--- components/header.js
+++ components/header.js
@@ -271,7 +271,7 @@ const Header = () => {
   return (
     <div className="flex items-center justify-between p-5 border-b border-gray-100">
       <Link href="/">
-        <SunWaveDisjoin className="w-12 h-12 text-red-500" />
+        <SunLines className="w-12 h-12 text-red-500" />
       </Link>
       <div className="flex space-x-4">
         <Link href="/words">Words</Link>
