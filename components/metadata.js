e478d2b4878e7096c8051829c6d5e62e39a1db67
Devin McGloin <devin@devinmcgloin.com>
2020-10-17 19:23:40 +0100

    better metadata

--- components/metadata.js
+++ components/metadata.js
@@ -89,18 +89,18 @@ const StandardMetadata = ({ title, description }) => (
     <meta name="msapplication-square310x310logo" content="mstile-310x310.png" />
     <meta
       name="twitter:image"
-      content="https://www.arenahq.io/arena-social.png"
+      content="https://devinmcgloin.com/assets/logo.png"
     />
     <meta
       property="og:image"
-      content="https://www.arenahq.io/arena-social.png"
+      content="https://devinmcgloin.com/assets/logo.png"
     />
     <meta property="og:image:height" content="650" />
-    <meta property="og:image:width" content="1200" />
-    <meta name="twitter:card" content="summary_large_image" />
+    <meta property="og:image:width" content="650" />
+    <meta name="twitter:card" content="summary_image" />
     <meta property="og:title" content={title} key="og-title" />
     <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://www.arenahq.io" />
+    <meta property="og:url" content="https://devinmcgloin.com" />
     <meta
       property="og:description"
       content={description}
