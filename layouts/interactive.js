2a0e4e51baeba20d62c4149f1b280a58003e45cf
Devin McGloin <devin@devinmcgloin.com>
2020-10-18 14:57:12 +0100

    Fix spacing

--- layouts/interactive.js
+++ layouts/interactive.js
@@ -33,13 +33,13 @@ const InteractiveLayout = ({ title, description, mountEditor }) => {
       <canvas className="display-block overflow-hidden" id="canvas" />
       <div className="absolute font-serif text-white font-normal italic md:left-16 md:bottom-16 bottom-10 left-10 p-5 md:max-w-sm max-w-xs bg-opacity-25 bg-black rounded-sm">
         <div className="flex items-center space-x-2">
-          <h1 className="text-3xl">{title}</h1>
+          <h1 className="md:text-3xl text-xl">{title}</h1>
           <span>·</span>
           <Link href="/art">
             <a className="border-b border-dotted text-sm">View All</a>
           </Link>
         </div>
-        <span>{description}</span>
+        <span className="md:text-normal text-sm">{description}</span>
       </div>
     </main>
   );
