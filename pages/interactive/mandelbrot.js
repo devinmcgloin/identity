1ed2e5ce7e667cceff70ee55efaf78159e3bb233
Devin McGloin <devin@devinmcgloin.com>
2020-10-19 00:35:50 +0100

    tweaks

--- pages/interactive/mandelbrot.js
+++ pages/interactive/mandelbrot.js
@@ -15,68 +15,68 @@ const shaders = Shaders.create({
     frag: GLSL`
     precision highp float;
     varying vec3 vPos;       // Pixel position
-uniform float u_aspect;
+    uniform float u_aspect;
 
-uniform int u_iterations;
-uniform float u_variation;
-uniform float u_threshold;
-uniform vec3 u_color;
-uniform bool u_smooth_coloring;
+    uniform int u_iterations;
+    uniform float u_variation;
+    uniform float u_threshold;
+    uniform vec3 u_color;
+    uniform bool u_smooth_coloring;
 
-vec3 simple_mandelbrot(vec2 p){
-    vec3 c = vec3(0.0, 0.0, 0.0);
-    float l = 1.0;
-    vec2 z = vec2(0.0);
-    for(int i = 0 ; i < 1000000; i++)
-    {
-        z = vec2( z.x*z.x - z.y*z.y, 2.0*z.x*z.y ) + p;
-        if( dot(z,z) > u_threshold)
+    vec3 simple_mandelbrot(vec2 p){
+        vec3 c = vec3(0.0, 0.0, 0.0);
+        float l = 1.0;
+        vec2 z = vec2(0.0);
+        for(int i = 0 ; i < 1000000; i++)
         {
-            c += 0.5 + 0.5 * cos(l * u_variation + u_color);
-            break;
+            z = vec2( z.x*z.x - z.y*z.y, 2.0*z.x*z.y ) + p;
+            if( dot(z,z) > u_threshold)
+            {
+                c += 0.5 + 0.5 * cos(l * u_variation + u_color);
+                break;
+            }
+            if( i > u_iterations)
+            {
+                break;
+            }
+            l += 1.0;
         }
-        if( i > u_iterations)
-        {
-            break;
-        }
-        l += 1.0;
+        return c;
     }
-    return c;
-}
 
-vec3 smooth_mandelbrot(vec2 p){
-    vec3 c = vec3(0.0, 0.0, 0.0);
-    float l = 1.0;
-    vec2 z = vec2(0.0);
-    for(int i = 0 ; i < 1000000; i++)
-    {
-        z = vec2( z.x*z.x - z.y*z.y, 2.0*z.x*z.y ) + p;
-        if( dot(z,z) > u_threshold)
+    vec3 smooth_mandelbrot(vec2 p){
+        vec3 c = vec3(0.0, 0.0, 0.0);
+        float l = 1.0;
+        vec2 z = vec2(0.0);
+        for(int i = 0 ; i < 1000000; i++)
         {
-            float modulus = sqrt(dot(z, z));
-            float mu = l - (log(log(modulus))) / log(2.0);
-            c += 0.5 + 0.5 * cos(mu * u_variation + u_color);
-            break;
+            z = vec2( z.x*z.x - z.y*z.y, 2.0*z.x*z.y ) + p;
+            if( dot(z,z) > u_threshold)
+            {
+                float modulus = sqrt(dot(z, z));
+                float mu = l - (log(log(modulus))) / log(2.0);
+                c += 0.5 + 0.5 * cos(mu * u_variation + u_color);
+                break;
+            }
+            if( i > u_iterations)
+            {
+                break;
+            }
+            l += 1.0;
         }
-        if( i > u_iterations)
-        {
-            break;
+        return c;
+    }
+    void main() {
+        vec3 nvPos = vPos;
+        nvPos.x = vPos.x / u_aspect;
+        nvPos.x -= 0.5;
+        if(u_smooth_coloring){
+            gl_FragColor = vec4( smooth_mandelbrot(nvPos.xy), 1.0 );
+        } else {
+            gl_FragColor = vec4( simple_mandelbrot(nvPos.xy), 1.0 );
         }
-        l += 1.0;
     }
-    return c;
-}
-
-void main() {
-    vec3 nvPos = vPos;
-    nvPos.x = vPos.x / u_aspect;
-    nvPos.x -= 0.5;
-    if(u_smooth_coloring){
-        gl_FragColor = vec4( smooth_mandelbrot(nvPos.xy), 1.0 );
-    } else {
-        gl_FragColor = vec4( simple_mandelbrot(nvPos.xy), 1.0 );
-    }
-}`,
+    `,
   },
 });
 
@@ -97,11 +97,15 @@ class Mandelbrot extends Component {
   render = () => {
     return (
       <InteractiveLayout title="Mandelbrot" mountEditor={this.mountEditor}>
-        <Surface width={900} height={900}>
+        <Surface
+          width={900}
+          height={900}
+          webglContextAttributes={{ alpha: false }}
+        >
           <Node
             shader={shaders.mandelbrot}
             uniforms={{
-              u_aspect: 1.5,
+              u_aspect: 1,
               u_iterations: this.iterations,
               u_variation: this.variation,
               u_color: this.color,
