9ab5b25ad26ea81d27d7dede8696bc08e90b9b78
Devin McGloin <devin@devinmcgloin.com>
2020-12-03 18:24:25 -0800

    Fix snippet

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { useEffect } from 'react';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -14,11 +15,19 @@ class MyDocument extends Document {
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-KY2C22E7E4"
           ></script>
-          <script>
-            window.dataLayer = window.dataLayer || []; function gtag()
-            {dataLayer.push(arguments)}
-            gtag('js', new Date()); gtag('config', 'G-KY2C22E7E4');
-          </script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag() {
+                  dataLayer.push(arguments);
+                }
+                gtag('js', new Date());
+
+                gtag('config', 'G-KY2C22E7E4');
+              `,
+            }}
+          />
         </Head>
         <body className="dark:bg-off-black">
           <Main />
