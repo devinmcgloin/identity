b3e1f4e7028b7c022c211a09b47905f49e1268fe
Devin McGloin <devin@devinmcgloin.com>
2020-12-03 18:20:09 -0800

    Add tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,17 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-KY2C22E7E4"
+          ></script>
+          <script>
+            window.dataLayer = window.dataLayer || []; function gtag()
+            {dataLayer.push(arguments)}
+            gtag('js', new Date()); gtag('config', 'G-KY2C22E7E4');
+          </script>
+        </Head>
         <body className="dark:bg-off-black">
           <Main />
           <NextScript />
